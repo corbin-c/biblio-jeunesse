8ad4707395119c1eabba2ddbdf1590c86a84bec3
Clément Corbin <corbin.clement@gmail.com>
2020-08-09 17:25:40 +0200

    book view

--- vue/book.vue.js
+++ vue/book.vue.js
@@ -1,13 +1,160 @@
+import { vLink } from "./vlink.vue.js";
+
 let bookView = {
   data: function() {
     return {
-      book: this.$root.collection.find(e => e.id == this.id)
+      book: this.$root.collection.find(e => e.id == this.id),
+      zoom: false,
+      summaryShown: 0,
+      contributors: this.$root.authors.findByContributions(this.id)
+    }
+  },
+  components: {
+    "v-link": vLink
+  },
+  methods: {
+    getRole(contributor) {
+      console.log(contributor);
+      return this.$root.authors
+        .getRole(contributor.contributions.find(e => e.bookId == this.id).role);
+    },
+    renderHTML (string) {
+      let p = document.createElement("p");
+      p.innerHTML = string;
+      let output = p.innerText;
+      p.remove();
+      return output;
+    },
+    changeSummary() {
+      this.summaryShown = (this.summaryShown+1)%this.book.summary.length
+    },
+    enlarge(e) {
+      if (this.zoom) {
+        this.zoom = false;
+      } else {
+        if (e.target.id == "thumbnail") {
+        this.zoom = true;          
+        }
+      }
+    },
+    getTagClass() {
+      let colors = ["primary", "secondary", "success", "danger", "info", "warning","dark"];
+      return "mx-2 my-1 btn btn-outline-"
+        +colors[Math.floor(Math.random()*colors.length)];
+    },
+    getClass(element) {
+      let classes;
+      if (this.zoom) {
+        classes = {
+          imgContainer: "img-container",
+          content: "card d-md-flex flex-md-row m-3 blur"
+        };
+      } else {
+        classes = {
+          imgContainer: "d-none",
+          content: "card d-md-flex flex-md-row m-3",
+        };
+      }
+      return classes[element];
     }
   },
   mounted: function() {
     window.document.title = this.$root.appName+" - "+this.book.title;
   },
   props: ["id"],
-  template: `<p>{{ book.title }} {{ book.publisher.name }} {{ book.allPublishers.join(" / ") }}</p>`
+  template: `
+  <section v-on:click="enlarge" v-bind:class="getClass('section')">
+    <article v-bind:class="getClass('imgContainer')">
+      <div class="img-thumbnail p-3 mx-auto">
+        <img
+          v-if="typeof book.cover !== 'undefined'"
+          v-bind:src="book.cover"
+          class="rounded">
+      </div>
+    </article>
+    <article v-bind:class="getClass('content')">
+      <img
+        class="rounded col-md-3 col-lg-2 col-xl-1 p-0"
+        v-if="typeof book.cover !== 'undefined'"
+        v-bind:src="book.cover"
+        id="thumbnail" style="cursor: zoom-in;">
+      <div class="col-md-9 col-lg-10 col-xl-11 p-0">
+        <h3 class="card-header text-secondary">{{ book.title }}</h3>
+        <h5 class="mt-4">
+          <ul class="inline mb-0">
+            <li v-for="(author,index) in book.authors.raw" v-bind:key="author">
+              <v-link 
+                v-bind:href="'author/'+author.replace(/ /g,'_')"
+                v-bind:title="'Consulter la fiche de '+author">
+                {{ author }}
+              </v-link><span v-if="index+1 < book.authors.raw.length"> & </span>
+            </li>
+          </ul>
+        </h5>
+        <div class="card-body border-bottom">
+          <button
+            title="Afficher un autre résumé"
+            class="m-2 btn btn-outline-primary svg-btn float-right"
+            v-on:click="changeSummary">
+            <svg class="d-inline">
+              <use xlink:href="/octicons-sprite/octicons-sprite.svg#sync-24"></use>
+            </svg>
+          </button>
+          <p class="card-text">{{ renderHTML(book.summary[summaryShown]) }}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li
+            v-if="contributors.length > 0"
+            class="list-group-item"><strong>Les auteurs :</strong></li>
+          <li v-for="contributor in contributors" class="list-group-item pl-5">
+              <v-link 
+                v-bind:href="'author/'+contributor.preferredName.replace(/ /g,'_')"
+                v-bind:title="'Consulter la fiche de '+contributor.preferredName">
+                {{ contributor.preferredName }}
+              </v-link><span class="text-secondary">({{ getRole(contributor) }})</span>
+          </li>
+          <li
+            v-if="book.description.length > 0"
+            class="list-group-item"><strong>Format :</strong>
+              {{ book.description.join(", ") }}
+          </li>
+          <li
+            v-if="book.publisher.name.length > 0"
+            class="list-group-item"><strong>Nombre de pages :</strong>
+              {{ book.pages.sort((a,b) => b.length - a.length)[0] }}
+          </li>
+          <li
+            v-if="book.publisher.name.length > 0"
+            class="list-group-item"><strong>Éditeur :</strong>
+              {{ book.publisher.name }}
+          </li>
+          <li
+            v-if="book.collection.length > 0"
+            class="list-group-item"><strong>Collection :</strong>
+              {{ book.collection.sort((a,b) => a.length - b.length)[0] }}
+          </li>
+          <li
+            v-if="book.price.length > 0"
+            class="list-group-item"><strong>Prix :</strong>
+              {{ book.price[0] }}
+          </li>
+          <li
+            v-if="book.tags.length > 0"
+            class="list-group-item"><strong>Mots-clés :</strong>
+              <ul class="d-inline inline p-0">
+                <li v-for="tag in book.tags">
+                  <v-link
+                    v-bind:href="'tag/'+tag.replace(/ /g,'_')"
+                    v-bind:title="'Consulter les livres associés à ce mot-clé : '+tag"
+                    v-bind:class="getTagClass()">
+                    {{ tag }}
+                  </v-link>
+                </li>
+              </ul>
+          </li>
+        </ul>
+      </div>
+    </article>
+  </section>`
 };
 export { bookView }
